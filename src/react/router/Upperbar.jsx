2f85149406bf06006ff699bf50a43cd1bd3072ce
Jhoseph <jhosephgp@gmail.com>
2020-09-28 22:38:33 -0400

    split menu icon funtionability into its own component and made upperbar component cleaner fix a detail

--- src/react/router/Upperbar.jsx
+++ src/react/router/Upperbar.jsx
@@ -6,15 +6,13 @@ import MenuHandler from './MenuHandler';
 import DolarPortalHandler from './DolarPortalHandler';
 import Logout from './Logout';
 
-
-
 const UpperBar = () => {
   const dataLoaded = useSelector(state => state.login.dataLoaded);
   if (!dataLoaded) return null;
 
   return (
     <div className="upper_bar">
-      </MenuHandler />
+      <MenuHandler />
       <DolarPortalHandler />
       <Logout />
     </div>
