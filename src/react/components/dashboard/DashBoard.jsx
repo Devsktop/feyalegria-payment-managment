74dc78b800b07457ba433890f5145de20aad67df
Jhoseph Guerrero <jhosephgp@gmail.com>
2020-10-08 17:44:13 -0400

    create redux store for students

--- src/react/components/dashboard/DashBoard.jsx
+++ src/react/components/dashboard/DashBoard.jsx
@@ -1,55 +1,13 @@
 import React from 'react';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 // Components
-import DataBox from './DataBox';
+import DataBoxContainer from './DataBoxContainer';
 import ChartContainer from './ChartContainer';
 
 const Dashboard = () => {
   return (
     <div className="dashboard content-screen">
-      <DataBox
-        icon={faUsers}
-        desc="Alumnos inscritos"
-        data="60"
-        link="/register"
-      />
-      <DataBox
-        icon={faUsers}
-        desc="Alumnos inscritos"
-        data="60"
-        link="/register"
-      />
-      <DataBox
-        icon={faUsers}
-        desc="Alumnos inscritos"
-        data="60"
-        link="/register"
-      />
-      <DataBox
-        icon={faUsers}
-        desc="Alumnos inscritos"
-        data="60"
-        link="/register"
-      />
-      <DataBox
-        icon={faUsers}
-        desc="Alumnos inscritos"
-        data="60"
-        link="/register"
-      />
-      <DataBox
-        icon={faUsers}
-        desc="Alumnos inscritos"
-        data="60"
-        link="/register"
-      />
-      <DataBox
-        icon={faUsers}
-        desc="Alumnos inscritos"
-        data="60"
-        link="/register"
-      />
+      <DataBoxContainer />
       <ChartContainer />
     </div>
   );
