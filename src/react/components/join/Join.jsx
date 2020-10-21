95d8dc21d3a022a62e6552b3f176e4a3e9761c0b
Jhoseph Guerrero <jhosephgp@gmail.com>
2020-10-21 18:22:11 -0400

    split value pair logic form join component a put pair value components insaide it's own folder

--- src/react/components/join/Join.jsx
+++ src/react/components/join/Join.jsx
@@ -1,55 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { createSelector } from 'reselect';
 
 // Helper
 import { decimalValidator } from 'helper';
 
-// Actions
-import {
-  addConceptsInscription,
-  updateConceptInscription,
-  deleteConceptInscription
-} from 'react/redux/actions/conceptsActions';
-
 // Components
 import Minput from 'react/components/Minput';
-import AddValuePair from './AddValuePair';
-
-// Selectors
-const boxSelector = state => {
-  const { concepts } = state;
-  let paymentConcepts = {};
-
-  Object.keys(concepts).forEach(concept => {
-    if (concepts[concept].type === 'INSCRIPTION')
-      paymentConcepts = { ...concepts[concept].paymentConcepts };
-  });
-
-  const paymentConceptsKeys = {};
-
-  Object.keys(paymentConcepts).forEach(concept => {
-    paymentConceptsKeys[concept] = concept;
-  });
-
-  return paymentConceptsKeys;
-};
-
-const joinConceptsSelector = createSelector(
-  state => {
-    const { concepts } = state;
-    let paymentConcepts = {};
-
-    Object.keys(concepts).forEach(concept => {
-      if (concepts[concept].type === 'INSCRIPTION')
-        paymentConcepts = { ...concepts[concept].paymentConcepts };
-    });
-
-    return paymentConcepts;
-  },
-  (_, id) => id,
-  (paymentConcepts, id) => paymentConcepts[id]
-);
+import JoinValuePair from './JoinValuePair';
 
 const Join = () => {
   const [price, setPrice] = useState('');
@@ -73,17 +30,8 @@ const Join = () => {
           value={price}
           label="Ingrese precio de la matrícula:"
         />
-        <AddValuePair
-          boxSelector={boxSelector}
-          addPairAction={addConceptsInscription}
-          changePairAction={updateConceptInscription}
-          pairSelector={joinConceptsSelector}
-          removePairAction={deleteConceptInscription}
-          pairKeys={['concept', 'price']}
-          valueDecimal
-          pairLabels={['Concepto', 'Precio']}
-        />
 
+        <JoinValuePair />
         <div className="button_container">
           <Link className="button button-cancel" to="/Config">
             Volver
---
+++ src/react/components/join/JoinValuePair.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { createSelector } from 'reselect';
+// Actions
+import {
+  addConceptsInscription,
+  updateConceptInscription,
+  deleteConceptInscription
+} from 'react/redux/actions/conceptsActions';
+
+// Components
+import AddValuePair from '../valuePair/AddValuePair';
+
+// Selectors
+const boxSelector = state => {
+  const { concepts } = state;
+  let paymentConcepts = {};
+
+  Object.keys(concepts).forEach(concept => {
+    if (concepts[concept].type === 'INSCRIPTION')
+      paymentConcepts = { ...concepts[concept].paymentConcepts };
+  });
+
+  const paymentConceptsKeys = {};
+
+  Object.keys(paymentConcepts).forEach(concept => {
+    paymentConceptsKeys[concept] = concept;
+  });
+
+  return paymentConceptsKeys;
+};
+
+const joinConceptsSelector = createSelector(
+  state => {
+    const { concepts } = state;
+    let paymentConcepts = {};
+
+    Object.keys(concepts).forEach(concept => {
+      if (concepts[concept].type === 'INSCRIPTION')
+        paymentConcepts = { ...concepts[concept].paymentConcepts };
+    });
+
+    return paymentConcepts;
+  },
+  (_, id) => id,
+  (paymentConcepts, id) => paymentConcepts[id]
+);
+
+const JoinValuePair = () => {
+  return (
+    <AddValuePair
+      boxSelector={boxSelector}
+      addPairAction={addConceptsInscription}
+      changePairAction={updateConceptInscription}
+      pairSelector={joinConceptsSelector}
+      removePairAction={deleteConceptInscription}
+      pairKeys={['concept', 'price']}
+      valueDecimal
+      pairLabels={['Concepto', 'Precio']}
+    />
+  );
+};
+
+export default JoinValuePair;
