62ea8dfabdd6beeac2fe649134157eb56e48d1fa
Jhoseph Guerrero <jhosephgp@gmail.com>
2020-11-13 10:43:18 -0400

    dsf

--- src/shared/express/database.js
+++ src/shared/express/database.js
@@ -1,13 +1,8 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-<<<<<<< HEAD
   host: '192.168.0.17',
   user: 'jhoseph',
-=======
-  host: 'localhost',
-  user: 'root',
->>>>>>> 3e97067000d5deedf3ad31238266a48fd1c89348
   password: '',
   database: 'feyalegria'
 });
