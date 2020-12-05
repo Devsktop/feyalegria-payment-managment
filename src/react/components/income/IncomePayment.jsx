578ac3b190413f55d94273488453ada3ad6a6a50
Jhoseph Guerrero <jhosephgp@gmail.com>
2020-12-05 11:55:01 -0400

    add titles and styles to incomepayment

--- src/react/components/income/IncomePayment.jsx
+++ src/react/components/income/IncomePayment.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const IncomePayment = () => {
-  return <div className="incomepayment">hola</div>;
+  return (
+    <div className="incomepayment">
+      <h1 className="incomepayment_title">Pago de inscripción</h1>
+      <h2 className="incomepayment_subtitle">Ingrese los datos del pago</h2>
+    </div>
+  );
 };
 
 export default IncomePayment;
