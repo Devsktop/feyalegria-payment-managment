acb3092b021896208756e2c0e1170a387db434b3
Jhoseph Guerrero <jhosephgp@gmail.com>
2020-12-12 23:17:26 -0400

    now in payments box only show the payments that are checked

--- src/react/components/income/PaymentsBox.jsx
+++ src/react/components/income/PaymentsBox.jsx
@@ -6,12 +6,18 @@ import PaymentsTrasference from './PaymentsTrasference';
 import PaymentsCash from './PaymentsCash';
 import PaymentsDolar from './PaymentsDolar';
 
+const showPaymentsSelector = state => {
+  const { transference, dolar, cash } = state.income.incomeBalance;
+  return { transference, dolar, cash };
+};
+
 const PaymentsBox = () => {
+  const showPayments = useSelector(showPaymentsSelector);
   return (
     <div>
-      <PaymentsTrasference />
-      <PaymentsCash />
-      <PaymentsDolar />
+      {showPayments.transference && <PaymentsTrasference />}
+      {showPayments.cash && <PaymentsCash />}
+      {showPayments.dolar && <PaymentsDolar />}
     </div>
   );
 };
