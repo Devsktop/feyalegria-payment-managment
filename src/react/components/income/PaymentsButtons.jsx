e1c0efd9dd89aeff925a4c5d60cb05caab748abf
jhoseph <jhoseph@gmail.com>
2021-05-17 20:57:56 -0400

    validate payment buttons inputs

--- src/react/components/income/PaymentsButtons.jsx
+++ src/react/components/income/PaymentsButtons.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 
 // components
 import Button from 'react/components/Button';
 
+const disableButtonSelector = (state) => {
+  const { transference, transferenceAmount, transferenceRef, cash, cashAmount, dolar, dolarAmount } = state.income.incomeBalance;
+  let disable = false;
+  if (transference && transferenceRef === "" || transferenceAmount < 1) disable = true;
+  if (cash && cashAmount < 1) disable = true;
+  if (dolar && dolarAmount < 1) disable = true;
+  return disable;
+}
+
 const JoinStudentsButton = () => {
   const history = useHistory();
+  const disable = useSelector(disableButtonSelector);
+
+  const handleOnClick = () => {
+    history.push("/purchaseProducts")
+  }
   return (
     <div className="payments_buttons">
       <Button text="Volver" onClick={() => history.goBack()} />
-      <Button link="/purchaseProducts" text="Inscribir" />
+      <Button onClick={handleOnClick} text="Inscribir" disabled={disable} />
     </div>
   );
 };
