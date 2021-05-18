57625ed1f4cffaa78af840ba6d57ae8ea103eee0
jhoseph <jhoseph@gmail.com>
2021-05-18 15:42:50 -0400

    Fix IncomePaymentButtons condintional bug

--- src/react/components/income/PaymentsButtons.jsx
+++ src/react/components/income/PaymentsButtons.jsx
@@ -8,7 +8,7 @@ import Button from 'react/components/Button';
 const disableButtonSelector = (state) => {
   const { transference, transferenceAmount, transferenceRef, cash, cashAmount, dolar, dolarAmount } = state.income.incomeBalance;
   let disable = false;
-  if (transference && transferenceRef === "" || transferenceAmount < 1) disable = true;
+  if (transference && (transferenceRef === "" || transferenceAmount < 1)) disable = true;
   if (cash && cashAmount < 1) disable = true;
   if (dolar && dolarAmount < 1) disable = true;
   return disable;
